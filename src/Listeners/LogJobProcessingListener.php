b012d698c85c5b6cca8be623dd87cc27ca867f85
alfan06 <alfannoufal@gmail.com>
2025-06-12 11:48:56 +0700

    v0.5.0

---
+++ src/Listeners/LogJobProcessingListener.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AdevPmftc\LaravelHealthcheck\Listeners;
+
+use Illuminate\Queue\Events\JobProcessing;
+use Illuminate\Support\Facades\Log;
+
+class LogJobProcessingListener
+{
+    /**
+     * Handle the event.
+     */
+    public function handle(JobProcessing $event): void
+    {
+        $payload = $event->job->payload();
+
+        Log::info('Job Processing', [
+            'event' => 'job.processing',
+            'job' => $payload['displayName'] ?? 'Unknown Job',
+            'connection' => $event->connectionName,
+            'queue' => $event->job->getQueue(),
+            'payload' => $payload,
+        ]);
+    }
+}
